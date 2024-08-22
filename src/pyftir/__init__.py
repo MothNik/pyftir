d2899f92e94bcc5410d14543766c7f74166c2d0c
MothNik <nik.zoe@web.de>
2024-08-22 18:12:57 +0200

    ENH: - setup `src`-folder - added author and version to package

---
+++ src/pyftir/__init__.py
@@ -0,0 +1,21 @@
+"""
+Package ``pyftir``
+
+A Python package for Fourier Transform Spectroscopy in Python.
+
+"""
+
+# === Imports ===
+
+import os as _os
+
+# === Package Metadata ===
+
+_AUTHOR_FILE_PATH = _os.path.join(_os.path.dirname(__file__), "AUTHORS.txt")
+_VERSION_FILE_PATH = _os.path.join(_os.path.dirname(__file__), "VERSION.txt")
+
+with open(_AUTHOR_FILE_PATH, "r") as author_file:
+    __author__ = author_file.read().strip()
+
+with open(_VERSION_FILE_PATH, "r") as version_file:
+    __version__ = version_file.read().strip()
